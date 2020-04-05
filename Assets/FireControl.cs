1b1b14538a6492d804eaf35720af3a01c5a56fdb
Velcronator <valacos7@gmail.com>
2020-04-05 21:49:44 +1000

    L24 Shooting Bullets Part 3. Transfered the Bullet code to run on the clients to get rid of the lag by sending it the the server and back again.

--- Assets/FireControl.cs
+++ Assets/FireControl.cs
@@ -19,13 +19,24 @@ public class FireControl : NetworkBehaviour
         }
     }
 
+    void CreateBullet()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, bulletSpawn.transform.position, bulletSpawn.transform.rotation);
+        bullet.GetComponent<Rigidbody>().velocity = bulletSpawn.transform.forward * 50;
+        Destroy(bullet, 3.0f);
+    }
+
+    [ClientRpc]// All ClientRPC classes need to start with Rpc.....
+    void RpcCreateBullet()
+    {
+        if (!isServer)
+            CreateBullet();
+    }
+
     [Command]
     void CmdShoot()
     {
-        GameObject bullet = Instantiate(bulletPrefab, bulletSpawn.transform.position, bulletSpawn.transform.rotation);
-        //bullet.GetComponent<Rigidbody>().AddForce(bulletSpawn.transform.forward * 2000);
-        bullet.GetComponent<Rigidbody>().velocity = bulletSpawn.transform.forward*50;
-        NetworkServer.Spawn(bullet);
-        Destroy(bullet, 3.0f);
+        CreateBullet();
+        RpcCreateBullet();
     }
 }
